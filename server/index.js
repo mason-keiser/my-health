c057eeba4620154f0b6938276a6d601a62ec60d2
Mason Keiser <masonksr5@gmail.com>
2021-01-05 19:59:42 +0000

    backend users

--- server/index.js
+++ server/index.js
@@ -19,6 +19,59 @@ app.get('/api/health-check', (req, res, next) => {
     .catch(err => next(err));
 });
 
+//  SIGN UP API POST REQUEST THAT ADDS USER INFO TO DB
+
+app.post('/api/signUp/', (req, res, next) => {
+  if (!req.body.firstname) {
+    return next(new ClientError('Missing required firstname field', 400));
+  }
+  if (!req.body.lastname) {
+    return next(new ClientError('Missing required lastname field', 400));
+  }
+  if (!req.body.email) {
+    return next(new ClientError('Missing required user_email field', 400));
+  }
+  if (!req.body.password) {
+    return next(new ClientError('Missing required user_password field', 400));
+  }
+  const sql = `
+  INSERT INTO "users" ("firstname", "lastname", "email", "password")
+  VALUES                  ($1, $2, $3, $4)
+  RETURNING *;
+  `;
+  const params = [req.body.firstname, req.body.lastname, req.body.email, req.body.password];
+  db.query(sql, params)
+    .then(result => {
+      const row = result.rows[0];
+      res.status(201).json(row);
+    });
+});
+
+//  SEARCH DATABASE FOR EXISTING EMAIL AND PASSWORD API GET REQUEST
+
+app.get('/api/login/:email/:password', (req, res, next) => {
+  const email = req.params.email;
+  const password = req.params.password;
+  const sql = `
+  SELECT * FROM "users"
+  WHERE "email" = $1 
+  AND "password" = $2;
+  `;
+  const params = [req.params.email, req.params.password];
+  db.query(sql, params)
+    .then(result => {
+      if (!result.rows[0]) {
+        return res.status(400).json({ message: `No user information contains: ${email} or ${password}` });
+      } else {
+        return res.status(200).json(result.rows);
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'An unexpected error occurred.' });
+    });
+});
+
 app.use('/api', (req, res, next) => {
   next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
 });
