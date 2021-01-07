ba381588ebba9a27ce0ba2560e33433e94e9fd32
Mason Keiser <masonksr5@gmail.com>
2021-01-07 17:13:33 +0000

    removed unnecessary date code

--- client/components/main.jsx
+++ client/components/main.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 function dateBuilder(d) {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let day = days[d.getDay()];
     let date = d.getDate();
     let month = months[d.getMonth()];
     let year = d.getFullYear();
