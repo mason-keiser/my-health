be169dd9187eca1da9388b919abe12664a31ab5a
Mason Keiser <masonksr5@gmail.com>
2021-01-07 19:57:45 +0000

    created color obj

--- client/components/pain_card.jsx
+++ client/components/pain_card.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
+function matchId_to_Color(props) {
+    const colorObj = {
+        1: '#08DB37', 2: '#98F877', 3: '#DBFF00', 4: '#F8FD00',
+        5: '#FFAD0F', 6: '#EE731A', 7: '#EC3E07', 8: '#FF0000',
+        9: '#08DB37', 10: '#98F877', 11: '#DBFF00', 12: '#F8FD00',
+        13: '#FFAD0F', 14: '#EE731A', 15: '#EC3E07', 16: '#FF0000'
+    }
+}
+
 export default function Pain_Card (props) {
     return (
         <div>
