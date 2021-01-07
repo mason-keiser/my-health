627e23eee4ef1044c31198eda047339c83512979
Mason Keiser <masonksr5@gmail.com>
2021-01-07 21:49:53 +0000

    added another btn

--- client/components/pain_history.jsx
+++ client/components/pain_history.jsx
@@ -54,7 +54,7 @@ export default class Pain_History extends React.Component {
                 <h1 className="paintitle" onClick={() => this.props.setView('main',{})}>My Health ♡</h1>
                 <div className="backbuttons m-4">
                     <h4 onClick={() => this.props.setView('main',{})}>Home</h4>
-                    <h4 style={{color: 'white'}} onClick={() => this.props.setView('pain_history',{})}>History</h4>
+                    <h4 onClick={() => this.props.setView('pain',{})}>Pain Today</h4>
                 </div>
                 <div>
                     {list}
