352337d65de206e72799b139c7cb4eefe5ed0c1f
Mason Keiser <masonksr5@gmail.com>
2021-01-07 19:15:57 +0000

    fixed styling issue

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -14,7 +14,7 @@ export default class App extends React.Component {
       message: null,
       isLoading: true,
       view: {
-        name: 'main',
+        name: 'welcome',
         params: {}
       },
       user: {
