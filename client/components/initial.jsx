bcc112efad14165cff6a122f701ecf44971ea0ca
Mason Keiser <masonksr5@gmail.com>
2021-01-09 21:05:16 +0000

    landing page changes

--- client/components/initial.jsx
+++ client/components/initial.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default function Initial (props) {
     let ent = (window.screen.width > 400) 
-        ? <h5 onClick={() => props.setView('login', {})}>Click here to start</h5>
-        : <h5 onClick={() => props.setView('login', {})}>Tap here to start</h5>
+        ? <h5>Click to start</h5>
+        : <h5>Tap to start</h5>
     return (
-        <div className="initContainer">
+        <div onClick={() => props.setView('login', {})} className="initContainer">
             <h1 className="m-5">
                 My Health ♡
             </h1>
