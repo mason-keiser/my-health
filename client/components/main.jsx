25bdbc552aa95b3b40e80d40118993aa80c03b47
Mason Keiser <masonksr5@gmail.com>
2021-01-13 00:51:27 +0000

    more styling done to nav

--- client/components/main.jsx
+++ client/components/main.jsx
@@ -25,39 +25,14 @@ function dateBuilder(d) {
 export default class Main extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            isOpen: false
-          };
-          this.handleToggle = this.handleToggle.bind(this);
+        
     }
 
-
-    handleToggle() {
-        if (window.innerWidth < 768) {
-          this.setState({ isOpen: !this.state.isOpen });
-        }
-      }
-
     render() {
-        let offset = -86;
-        if (window.innerWidth < 768) {
-          offset = -286;
-        }
-        
-        var prevScrollpos = window.pageYOffset;
-        window.onscroll = function() {
-          var currentScrollPos = window.pageYOffset;
-          if (prevScrollpos > currentScrollPos) {
-            document.getElementById("navbar").style.top = "0";
-          } else {
-            document.getElementById("navbar").style.top = "-100px";
-          }
-        prevScrollpos = currentScrollPos;
-        }
         return (
             <div>
                 <Main_Page_Header/>
-                <h1 className="title" onClick={() => this.props.setView('main',{})}>My Health ♡</h1>
+                <h1 className="maintitle" onClick={() => this.props.setView('main',{})}>My Health ♡</h1>
                 <h2 className="date m-5">{dateBuilder(new Date())}</h2>
                 <h2 className="welcomb">Welcome back, {this.props.user.firstname}</h2>
                 <div className="mainB">
