9b6c275aa4e09eaf375b6cd4e2270aef8575d3fb
Mason Keiser <masonksr5@gmail.com>
2021-01-13 00:18:06 +0000

    fixed view

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -21,7 +21,7 @@ export default class App extends React.Component {
       message: null,
       isLoading: true,
       view: {
-        name: 'main',
+        name: 'init',
         params: {}
       },
       user: {
