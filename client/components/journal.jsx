5055802643da915baf6c2d5512d08528ec397c0d
Mason Keiser <masonksr5@gmail.com>
2021-01-13 01:23:24 +0000

    added nav to journal

--- client/components/journal.jsx
+++ client/components/journal.jsx
@@ -41,6 +41,7 @@ export default class Journal extends React.Component {
     render() {
         return (
             <div>
+                <Main_Page_Header setView={this.setView}/>
                 <h1 className="title" onClick={() => this.props.setView('main',{})}>My Health ♡</h1>
                 <div className="backbuttons m-4">
                     <h4 onClick={() => this.props.setView('main',{})}>Go Home</h4>
