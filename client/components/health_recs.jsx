ed848770694ae249b29c39327601e6b32412f8d5
Mason Keiser <masonksr5@gmail.com>
2021-01-17 00:52:51 +0000

    carousel working

--- client/components/health_recs.jsx
+++ client/components/health_recs.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Main_Page_Header from './main_page_header';
-import { Carousel } from 'react-responsive-carousel';
+import { NextArrow, BackArrow, Rec } from './health_recs_fx';
 
 export default class Health_Recs extends React.Component {
     constructor(props) {
@@ -26,7 +26,7 @@ export default class Health_Recs extends React.Component {
                     image: '/images/reading.png',
                     alt: 'Reading image',
                     name: 'Reading',
-                    description: 'A Hindu spiritual and ascetic discipline, a part of which, including breath control, simple meditation, and the adoption of specific bodily postures, is widely practiced for health and relaxation.'
+                    description: 'Reading books benefits both your physical and mental health, and those benefits can last a lifetime. They begin in early childhood and continue through the senior years.'
                 },
                 {
                     id: 4,
@@ -42,38 +42,81 @@ export default class Health_Recs extends React.Component {
                     name: 'Painting',
                     description: 'Taking up a hobby like painting isn’t just for the creative types. The more analytical left-brainers can stimulate and nurture their creative growth by painting as well. Practice and focus, two skills inherent in left-brain individuals, allow these people to learn creative skills at their own pace.'
                 },
-            ]
+            ],
+            activeIndex: 0
         }
+        this.goToNextRec = this.goToNextRec.bind(this);
+        this.goToPrevRec = this.goToPrevRec.bind(this);
+        this.shuffleRecArray = this.shuffleRecArray.bind(this);
     }
 
+    componentDidMount() {
+        this.shuffleRecArray();
+      }
+
+    shuffleRecArray() {
+        const shuffledArray = this.state.recs;
+        for (let index = shuffledArray.length - 1; index > 0; index--) {
+          const tempIndex = Math.floor(Math.random() * (index + 1));
+          [shuffledArray[index], shuffledArray[tempIndex]] = [shuffledArray[tempIndex], shuffledArray[index]];
+        }
+        this.setState({
+          recs: shuffledArray
+        });
+      }
+
+    goToNextRec() {
+        let index = this.state.activeIndex;
+        const length = this.state.recs.length;
+        if (index === length - 1) {
+          this.shuffleRecArray();
+          index = 0;
+        } else {
+          index++;
+        }
+        this.setState({
+          activeIndex: index
+        });
+      }
+
+    goToPrevRec() {
+        let index = this.state.activeIndex;
+        const length = this.state.recs.length;
+        if (index < 1) {
+          this.shuffleRecArray();
+          index = length - 1;
+        } else {
+          index--;
+        }
+        this.setState({
+          activeIndex: index
+        });
+      }
+
     render() {
-        const list = (this.state.recs !== undefined) 
-        ?  (this.state.recs.map((rec, index) => {
-                return(
-                    <div className='recs' key={rec.id}>
-                        <img
-                        className="recImg p-0"
-                        src={rec.image}
-                        alt={`${rec.name} poster`}
-                        />
-                        <h3 className='mt-3'>{rec.name}</h3>
-                        <h5 className='mb-3 pr-2 pl-2'>{rec.description}</h5>
-                    </div>
-                );
-            })
-        )
-        : null
         return (
             <div>
                 <Main_Page_Header setView = {this.props.setView}/>
                 <h1 className="title" onClick={() => this.props.setView('main',{})}>My Health ♡</h1>
-                <div className="backbuttons m-2">
+                <div className="backbuttons m-3">
                     <h4 onClick={() => this.props.setView('main',{})}>Go Home</h4>
                     <h4 style={{color: "white"}} onClick={() => this.props.setView('journal_history',{})}></h4>
                 </div>
                 <h2 className='title22 mt-4 mb-4'>Health Recommendations</h2>
-                <div style={{ width: "100%", margin: "auto"}}>
-                    {list}
+                <div className='recsRow'>
+                    <BackArrow className='col-1 ar' goToPrevRec={() => this.goToPrevRec()}/>
+                    <div style={{ width: "100%", margin: "auto"}}>
+                        {this.state.recs.map((rec, index) =>
+                        <Rec
+                            className='col-10'
+                            key={index}
+                            index={index}
+                            activeIndex={this.state.activeIndex}
+                            rec={rec}
+                        />
+                        )}
+                    </div>
+                    <NextArrow className='col-1 ar' goToNextRec={() => this.goToNextRec()}/>
                 </div>
             </div>
         )
